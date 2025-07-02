52dbc4c2615e791ec10fab956299557a820d999e
Hillary Malespin Ulloa <hillarymalespin@estudiantec.cr>
2025-07-02 00:52:56 -0600

    Avance

---
+++ ApiAzureDB/ApiAzureDB/DTOs/DocumentoDTO.cs
@@ -0,0 +1,7 @@
+public class DocumentoDto
+{
+    public int DocumentoID { get; set; }
+    public string Titulo { get; set; }
+    public string RutaAzureBlob { get; set; }
+    public DateTime FechaPublicacion { get; set; }
+}
