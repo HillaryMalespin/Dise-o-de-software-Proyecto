fb6f19a246babbe8263210096483fc7cfbabea90
Hillary Malespin Ulloa <hillarymalespin@estudiantec.cr>
2025-07-02 00:55:31 -0600

    Avance

---
+++ ARCAMovil/ARCAMovil/Models/Carpeta.cs
@@ -0,0 +1,8 @@
+namespace ARCAMovil.Models
+{
+    public class Carpeta
+    {
+        public int CarpetaID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ ARCAMovil/ARCAMovil/Models/CarpetaDto.cs
@@ -0,0 +1,8 @@
+namespace ARCAMovil.Models
+{
+    public class CarpetaDto
+    {
+        public int CarpetaTematicaID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
